be9ef017fb6160b27159ee67d1d3ff60f5fc6b83
Hamza Nadeem <nadeemmhamza@gmail.com>
2026-09-02 23:38:46 +0500

    feat : Lowest Common Ancestor of a BST

---
+++ lowest_common_ancestor_of_a_bst.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        # Base case: if root is None or root is either p or q
+        if not root or root == p or root == q:
+            return root
+        
+        # Search in left and right subtrees
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+        
+        # If both left and right return non-null, root is the LCA
+        if left and right:
+            return root
+        
+        # Otherwise, return the non-null side (or None if both are None)
+        return left if left else right
